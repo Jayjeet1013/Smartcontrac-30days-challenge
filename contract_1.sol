716276490a2b1439cea4447d53f2c01b1b4dcc09
Jayjeet1013 <jayjeet1013@gmail.com>
2023-04-27 21:25:05 +0530

    first commit

---
+++ contract_1.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity^0.8.18;
+
+contract AddTwoNumber{
+     
+        function addition(uint num1 , uint num2) pure public returns(uint){
+            return num1 + num2;
+        }
+}
