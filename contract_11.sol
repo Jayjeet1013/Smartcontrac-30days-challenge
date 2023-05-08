b15584b6c3a0f9260e10f25df93654c1661a64b2
Jayjeet1013 <jayjeet1013@gmail.com>
2023-05-08 23:13:40 +0530

    update

--- contract_11.sol
+++ contract_11.sol
@@ -2,7 +2,7 @@
 
 pragma solidity^0.8.17;
 
-contract transferAmount{
+contract transfer{
     event transaction(address from , address to , uint value);
     function transeferAmount(address payable receiver) public  payable {
         receiver.transfer(msg.value);
